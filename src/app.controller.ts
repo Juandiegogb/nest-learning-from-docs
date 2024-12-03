5f053df6c91bb25661465ae559ecf44aef35d06a
Juandiegogb <juandiegar@gmail.com>
2024-12-03 15:51:40 -0500

    testing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CreateCatDto } from './create-cat.dto';
 
-@Controller()
+@Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('createCat')
+  createCat(@Body() createCatDto: CreateCatDto) {
+    console.log(createCatDto);
   }
 }
