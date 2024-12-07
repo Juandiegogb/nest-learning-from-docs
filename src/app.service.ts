dce6783e613fa9190d0a2b50a976ae52ad97db13
Juandiegogb <juandiegar@gmail.com>
2024-12-07 11:44:39 -0500

    test #2 created

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  createCat(createCatDTO: CreateCatDto): string {
+    return JSON.stringify(createCatDTO);
   }
 }
